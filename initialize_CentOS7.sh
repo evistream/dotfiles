1ba961ea86ff677020960e9a9965dc1adabec979
evistream <evistream@yahoo.co.jp>
2019-09-25 20:41:51 +0900

    add script and dotfiles

---
+++ initialize_CentOS7.sh
@@ -0,0 +1,37 @@
+# 研究室で計算用サーバーを設定するためのファイル
+HOSTNAME=node2
+LOCAL_IP=157.82.23.245/24
+
+# selinux
+mv /etc/selinux/config{,.org}
+sed s/SELINUX=.*$/SELINUX=disabled/ /etc/selinux/config.org > /etc/selinux/config
+
+## ネットワーク	
+# nmcliを使用して設定。
+# 設定を修正する場合、nmcli con addの操作は冪等ではないのでnmtuiを使いマニュアルで再設定するべし。
+GW_IP=157.82.23.1
+CONNAME=ether1
+IFNAME=`nmcli device status | grep ethernet | head -n1 | awk '{print $1}'`
+nmcli con add type ethernet con-name $CONNAME ifname $IFNAME ip4 $LOCAL_IP \
+gw4 $GW_IP
+nmcli con mod $CONNAME ipv4.dns "133.11.123.45 133.11.124.164"
+nmcli con up $CONNAME ifname $IFNAME
+# ホスト名の変更
+OFFICENAME=nelab
+hostnamectl set-hostname $HOSTNAME.$OFFICENAME
+
+## タイムゾーン
+timedatectl set-local-rtc 0
+timedatectl set-timezone Asia/Tokyo
+
+## Firewall
+firewall-cmd --zone=public --add-port 8888/tcp --permanent
+firewall-cmd --reload
+
+## パッケージインストール
+yum install -y epel-release
+yum install -y zsh python36
+
+## ユーザーの初期設定
+chsh -s /bin/zsh node
+sudo -u node sh initialize_user.sh
---
+++ zshrc
@@ -0,0 +1,47 @@
+source ~/Documents/sakurayama_workspace/venv/bin/activate
+
+# prompt
+PS1="%% "
+autoload -Uz promptinit
+promptinit
+prompt bart red
+
+# color
+autoload -Uz colors
+colors
+
+# emacs-like key bindings
+bindkey -e
+
+# histroy
+HISTFILE=~/.zsh_history_sakura
+HISTSIZE=1000000
+SAVEHIST=1000000
+
+# kugiri wo word tanni ni
+autoload -Uz select-word-style
+select-word-style default
+
+# completion
+autoload -Uz compinit
+compinit
+
+# ignore case for completion
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# don't show current dir for completion after '../'
+zstyle ':completion:*' ignore-parents parent pwd ..
+ 
+setopt print_eight_bit
+setopt ignore_eof
+setopt interactive_comments
+setopt auto_cd
+setopt share_history
+setopt extended_glob
+
+bindkey '^R' history-incremental-pattern-search-backward
+
+alias la='ls -a'
+alias ll='ls -l'
+
+alias sakura='cd ~/Documents/sakurayama_workspace'
